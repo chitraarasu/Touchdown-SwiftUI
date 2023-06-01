47f3d40039e554682b1c96924a6a19f11f403566
Chitraarasu <98887175+chitraarasu-kirshi@users.noreply.github.com>
2023-06-01 07:32:38 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by kirshi on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
